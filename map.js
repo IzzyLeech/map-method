4ebef101011af551861756a32e157445b6697e2a
IzzyLeech <ralphhunter46@gmail.com>
2023-01-20 13:40:51 +0000

    map method

--- map.js
+++ map.js
@@ -6,15 +6,30 @@
 
 // Using a for loop
 
+let nums = [1,2,3,4,5];
+
+let result = [];
+for (let num of nums) {
+  result.push(num * 2);
+}
+
+console.log(result);
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multByTwo)
+console.log(mapResults)
 
 // Simplified w/ map()
 
-
+const simlified = nums.map(function (num) {return num * 2})
+console.log(simlified)
 // Simplfied w/ map() + arrow function
 
+const simplyMap = nums.map(n => n * 2);
+console.log(simplyMap)
 
 // With objects:
 const students = [
@@ -37,3 +52,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithId = students.map(student => [student.name, student.id])
+console.log(studentsWithId)
